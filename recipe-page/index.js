45a63d8bdfc9c1985e1b4a4cacc8d8abb6ddb24a
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-07-25 13:14:27 -0500

    Adds DOM manipulation that adds a message when the Done button is clicked

--- recipe-page/index.js
+++ recipe-page/index.js
@@ -1,8 +1,19 @@
+// Changes text color of step when step is clicked
 let colorCh = document.querySelector('ol');
 
     colorCh.addEventListener('click', function onClick(event) {
         console.log(event.target);
-      event.target.style.color = 'white';
+      event.target.style.color = 'rgba(243, 240, 231, 0.945)';
     });
 
-    
+// Adds a Done button that adds a congratulatory message when clicked
+let stepsDiv = document.querySelector('.steps');
+let doneBtn = document.createElement('button');
+doneBtn.innerHTML = "Done";
+stepsDiv.appendChild(doneBtn);
+
+doneBtn.addEventListener('click', function onClick(event) {
+  let msg = document.createElement('p');
+  msg.textContent = "Well done!";
+  doneBtn.after(msg);
+})
