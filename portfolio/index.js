6cd12092cd1a36c6a2d78b17f9ccd4ede0280969
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-04 01:25:51 -0500

    Beginning attempts at adding another song

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,15 @@
+// let music = document.querySelector('source');
+// let text = document.getElementById('now-playing');
+// let btn = document.createElement('button');
+// btn.innerText = "Change";
+// text.append(btn);
+// text.append(music);
+
+// document.addEventListener('click', (event) => {
+//     music.src = '15 Solar Sailer.wav';
+//     text.innerText = "Now Playing: Solar Sailer - Daft Punk";
+// })
+
 // Dropdown menu for form
 let states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 
@@ -6,6 +18,5 @@ let list = document.getElementById('address');
         let option = document.createElement('option');
         option.innerHTML = (states[i]);
         option.value = (states[i]);
-        console.log(option.value);
         list.append(option);
     }
