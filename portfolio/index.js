aad774baba2090fc7e237ca500cacf6ad5d09752
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-06 17:24:51 -0500

    Makes home section linkeable

--- portfolio/index.js
+++ portfolio/index.js
@@ -9,7 +9,7 @@
 //     music.src = '15 Solar Sailer.wav';
 //     text.innerText = "Now Playing: Solar Sailer - Daft Punk";
 // })
-
+//Now Playing: Selino - Tim Kahn
 // Dropdown menu for form
 let states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 
