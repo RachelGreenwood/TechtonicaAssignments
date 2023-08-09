39828a505c9539a1fe7e6919669004caed2f41f8
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-09 16:10:01 -0500

    When button is clicked, flips coin and displays result

--- coin-flip/index.js
+++ coin-flip/index.js
@@ -2,7 +2,7 @@
 
 // Flips coin and gives heads/tails 50% of the time each
 function coinFlip() {
-    let flip = Math.floor(Math.random());
+    let flip = Math.random();
     if (flip < 0.5) {
         return "Heads";
     } else {
@@ -18,3 +18,10 @@ let container = document.getElementById('coin-flip');
 let button = document.createElement('button');
 button.innerHTML = "Flip";
 container.append(button);
+let text = document.createElement('div');
+container.append(text);
+
+// When button is clicked, flip coin
+button.addEventListener('click', (event) => {
+    text.innerText = coinFlip();
+})
