a781224405d3c852902ea27a489bb91b0f94d945
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-22 00:52:19 -0500

    Hardcodes books data into Express route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const art = require('./BOOKS');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
+app.get('/', (req, res) => {
+  res.json(art);
+})
+
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`API server listening on port ${port}`);
   });
+
+  module.exports = app;
