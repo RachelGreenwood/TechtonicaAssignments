527e78668c22f23a8ff40a84c0087464ab7b39f5
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-22 20:05:45 -0500

    Yes

--- api/index.js
+++ api/index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
   res.json(art);
 })
 
+app.post('/', (req, res) => {
+  let arr = art;
+  console.log(req.body);
+  // arr.push(req);
+  res.json(arr);
+})
+
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`API server listening on port ${port}`);
